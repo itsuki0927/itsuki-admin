b230d9d1c1ec2500fe398b5a7d71a9bb41e99045
itsuki0927 <2309899048@qq.com>
2021-10-04 15:14:50 +0800

    chore: change article-detail buildOption -> option.transform

--- src/pages/article/components/ArticleDetail/Options.tsx
+++ src/pages/article/components/ArticleDetail/Options.tsx
@@ -1,41 +1,35 @@
 import { articleOrigins } from '@/constants/article/origin'
 import { ArticleOpen, articleOpens } from '@/constants/article/public'
 import { publishStates } from '@/constants/publish'
+import { getSelectOptionsByState } from '@/transforms/option'
 import ProCard from '@ant-design/pro-card'
 import { ProFormDependency, ProFormSelect, ProFormText } from '@ant-design/pro-form'
-import { Space } from 'antd'
-
-const buildOptions = (data: any[]) =>
-  data.map((item) => ({
-    value: item.id,
-    label: (
-      <Space>
-        {item.icon}
-        {item.name}
-      </Space>
-    ),
-  }))
 
 const Options = () => {
   return (
     <ProCard title='发布选项' headerBordered>
       <ProFormSelect
+        fieldProps={{
+          onChange: (value) => {
+            console.log('value:', value)
+          },
+        }}
         rules={[{ required: true, message: '请选择发布状态' }]}
-        options={buildOptions(publishStates)}
+        options={getSelectOptionsByState(publishStates)}
         labelAlign='left'
         label='发布状态'
         name='publish'
       />
       <ProFormSelect
         rules={[{ required: true, message: '请选择文章来源' }]}
-        options={buildOptions(articleOrigins)}
+        options={getSelectOptionsByState(articleOrigins)}
         labelAlign='left'
         label='文章来源'
         name='origin'
       />
       <ProFormSelect
         rules={[{ required: true, message: '请选择公开类型' }]}
-        options={buildOptions(articleOpens)}
+        options={getSelectOptionsByState(articleOpens)}
         labelAlign='left'
         label='公开类型'
         name='open'
