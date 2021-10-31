58ff0ab04a30a58318ec390745f197b8afa7356f
itsuki0927 <2309899048@qq.com>
2021-10-31 19:41:53 +0800

    chore: remove dev environment config

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -1,14 +1,7 @@
 // https://umijs.org/config/
 import { defineConfig } from 'umi'
 
-const { REACT_APP_ENV, VERSION } = process.env
-
 export default defineConfig({
-  define: {
-    TAG: REACT_APP_ENV,
-    BASE_PATH: 'http://localhost:5555',
-    VERSION: VERSION,
-  },
   plugins: [
     // https://github.com/zthxxx/react-dev-inspector
     'react-dev-inspector/plugins/umi/react-inspector',
