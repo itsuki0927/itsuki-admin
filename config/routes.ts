73113729d9bd876c40d056d498887a4ab93f9b4a
itsuki0927 <2309899048@qq.com>
2021-10-31 16:02:49 +0800

    chore: remove admin test page

--- config/routes.ts
+++ config/routes.ts
@@ -12,17 +12,6 @@ export default [
   },
   { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
   {
-    path: '/admin',
-    name: '管理页',
-    icon: 'crown',
-    access: 'canAdmin',
-    component: './Admin',
-    routes: [
-      { path: '/admin/sub-page', name: '二级管理页', icon: 'smile', component: './Welcome' },
-      { component: './404' },
-    ],
-  },
-  {
     path: '/article',
     name: '文章管理',
     icon: 'coffee',
