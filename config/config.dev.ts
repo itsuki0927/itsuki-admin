ad0d8eb3de4805ecfdff630152492f9b1bee3d32
itsuki0927 <2309899048@qq.com>
2021-11-05 19:08:46 +0800

    fix: fix editor use other lanuage unexpected usage

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -1,5 +1,6 @@
 // https://umijs.org/config/
 import { defineConfig } from 'umi'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
@@ -13,14 +14,14 @@ export default defineConfig({
     babelOptions: {},
   },
   chainWebpack: (config) => {
-    // config.entry('editor.worker').add('monaco-editor/esm/vs/editor/editor.worker.js')
-    // config.entry('json.worker').add('monaco-editor/esm/vs/language/json/json.worker')
-    // config.entry('css.worker').add('monaco-editor/esm/vs/language/css/css.worker')
-    // config.entry('html.worker').add('monaco-editor/esm/vs/language/html/html.worker')
-    // config.entry('ts.worker').add('monaco-editor/esm/vs/language/typescript/ts.worker')
-    // config.output
-    //   .filename('[name].bundle.js')
-    //   .globalObject('self')
-    //   .path(resolve(__dirname, '../dist'))
+    config.entry('editor.worker').add('monaco-editor/esm/vs/editor/editor.worker.js')
+    config.entry('json.worker').add('monaco-editor/esm/vs/language/json/json.worker')
+    config.entry('css.worker').add('monaco-editor/esm/vs/language/css/css.worker')
+    config.entry('html.worker').add('monaco-editor/esm/vs/language/html/html.worker')
+    config.entry('ts.worker').add('monaco-editor/esm/vs/language/typescript/ts.worker')
+    config.output
+      .filename('[name].bundle.js')
+      .globalObject('self')
+      .path(resolve(__dirname, '../dist'))
   },
 })
--- src/components/MarkdownEditor/monaco.ts
+++ src/components/MarkdownEditor/monaco.ts
@@ -3,21 +3,20 @@ import 'monaco-editor/esm/vs/basic-languages/markdown/markdown.contribution'
 export * from 'monaco-editor'
 
 // eslint-disable-next-line no-restricted-globals
-//  ;(self as any).MonacoEnvironment = {
-//    getWorkerUrl(moduleId: string, label: string) {
-//      console.log(moduleId, label)
-//      if (label === 'json') {
-//        return './json.worker.bundle.js'
-//      }
-//      if (label === 'css' || label === 'scss' || label === 'less') {
-//        return './css.worker.bundle.js'
-//      }
-//      if (label === 'html' || label === 'handlebars' || label === 'razor') {
-//        return './html.worker.bundle.js'
-//      }
-//      if (label === 'typescript' || label === 'javascript') {
-//        return './ts.worker.bundle.js'
-//      }
-//      return './editor.worker.bundle.js'
-//    },
-//  }
+;(self as any).MonacoEnvironment = {
+  getWorkerUrl(moduleId: string, label: string) {
+    if (label === 'json') {
+      return '../json.worker.bundle.js'
+    }
+    if (label === 'css' || label === 'scss' || label === 'less') {
+      return '../css.worker.bundle.js'
+    }
+    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+      return '../html.worker.bundle.js'
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return '../ts.worker.bundle.js'
+    }
+    return '../editor.worker.bundle.js'
+  },
+}
